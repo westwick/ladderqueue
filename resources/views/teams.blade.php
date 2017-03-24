70c23034069b872ede869572ad86d8b8c6dedd37
Andrew Westwick <andrew@funneldash.com>
2017-03-24 08:48:38 -0700

    don't show if no division

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -22,7 +22,12 @@
                                     <img class="team-logo-small" src="{{$team->logo}}" />
                                     {{$team->name}}
                                 </td>
-                                <td>Season 2 - {{$team->division_season2}}</td>
+                                @if($team->division_season2)
+                                    <td>Season 2 - {{$team->division_season2}}</td>
+                                @else
+                                    <td></td>
+                                @endif
+                                <td>{{$team->division_season2}}</td>
                                 <td>{{$team->owner->name}}</td>
                                 <td>{{$team->memberCount()}}/10</td>
                                 <td><a href="/team/{{$team->id}}">Join this team</a></td>
