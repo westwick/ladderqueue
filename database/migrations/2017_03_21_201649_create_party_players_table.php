409865da5f5de9721538597184c304bd1bb5170f
Andrew Westwick <andrew@funneldash.com>
2017-03-23 23:08:08 -0700

    dirty stupid typo

--- database/migrations/2017_03_21_201649_create_party_players_table.php
+++ database/migrations/2017_03_21_201649_create_party_players_table.php
@@ -16,7 +16,7 @@ class CreatePartyPlayersTable extends Migration
         Schema::create('party_players', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('party_id')->unsigned();
-            $table->foreign('party_id')->references('id')->on('partys');
+            $table->foreign('party_id')->references('id')->on('parties');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('status_id')->default(0);
