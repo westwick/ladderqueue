fae0000e062858b6650279ef039aee34dd294ee2
Andrew Westwick <andrew@funneldash.com>
2017-05-04 22:33:56 -0700

    basic forum page cleanup

--- resources/views/community/forum.blade.php
+++ resources/views/community/forum.blade.php
@@ -9,13 +9,19 @@
             <div class="new-post text-right">
                 <a class="new-post-btn button" href="/forum/post">New Post</a>
             </div>
-            <div class="panel nomargin">
+            <div class="panel">
                 @foreach($comments as $comment)
-                    <p>
-                        {{$comment->author->name}}: {{$comment->title}}
-                        <a href="/forum/post/{{$comment->id}}">View Post</a>
-                    </p>
-
+                    <div class="board-post">
+                        <p class="post-title">
+                            {{$comment->title}}
+                        </p>
+                        <p class="post-author">
+                            Posted by {{$comment->author->name}} {{$comment->created_at->diffForHumans()}}
+                        </p>
+                        <p class="post-comments">
+                            <a href="/forum/post/{{$comment->id}}">{{$comment->descendants()->count()}} comments</a>
+                        </p>
+                    </div>
                 @endforeach
             </div>
         </div>
