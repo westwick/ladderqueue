0fd055f0f9245ba359c0c7baa9a11cbdc2ce1acf
Andrew Westwick <andrew@funneldash.com>
2017-05-31 19:36:37 -0700

    add 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+    <style>
+        body {
+            background: #131313;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #1e7be4;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        img {
+            width: 200px;
+            height: auto;
+            margin-bottom: 20px;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <img src="/images/vxblue.png">
+        <div class="title">Be right back.</div>
+    </div>
+</div>
+</body>
+</html>
